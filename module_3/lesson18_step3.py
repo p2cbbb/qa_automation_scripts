6659d4e3e57c9f16b1359ebd1476029513d6b976
Denis Antonov <den69ant@yandex.ru>
2022-07-25 16:50:00 +0300

    completed module #3

--- module_3/lesson18_step3.py
+++ module_3/lesson18_step3.py
@@ -1,16 +1,8 @@
-# открыть страницу 
-# ввести правильный ответ 
-# нажать кнопку "Отправить" 
-# дождаться фидбека о том, что ответ правильный 
-# проверить, что текст в опциональном фидбеке полностью совпадает с "Correct!"
-
 import time
 import math
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture
@@ -24,15 +16,17 @@ def browser():
 @pytest.mark.parametrize('lesson', ['236895', '236896', '236897', '236898',
                                     '236899', '236903', '236904', '236905'])
 def test_solve_stepik_problem(browser, lesson):
+    # вычисляем правильный ответ
     answer = math.log(int(time.time()))
     link = f"https://stepik.org/lesson/{lesson}/step/1"
+    # открываем страницу
     browser.get(link)
     browser.implicitly_wait(5)
+    # вводим ответ в поле ответа
     browser.find_element(By.CSS_SELECTOR, 'div.quiz-component > textarea').send_keys(answer)
+    # находим и нажимаем на кнопку "Отправить"
     browser.find_element(By.CSS_SELECTOR, '.submit-submission').click()
-    # hint = WebDriverWait(browser, 12).until(
-    #     EC.text_to_be_present_in_element((By.CSS_SELECTOR, "p.smart-hints__hint"), 'Correct!')
-    # )
+    # ожидаем фидбека, проверяем корректность ответа
     hint = browser.find_element(By.CSS_SELECTOR, "p.smart-hints__hint")
     assert hint.text == "Correct!", "Feedback message are not 'Correct!'"
 
